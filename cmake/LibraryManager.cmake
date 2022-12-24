cae8f833bf8b4a8949e1ec51baa15487beb020f4
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-12-24 07:40:20 +0000

    debug

--- cmake/LibraryManager.cmake
+++ cmake/LibraryManager.cmake
@@ -115,14 +115,14 @@ macro(_semver)
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     OUTPUT_VARIABLE __GITDIR
-#                    ERROR_QUIET
+                    ERROR_QUIET
             )
             execute_process(
                     COMMAND ${GIT_EXECUTABLE} rev-parse --show-toplevel
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     OUTPUT_VARIABLE __GITTOP
-#                    ERROR_QUIET
+                    ERROR_QUIET
             )
             message(DEBUG "__GITDIR=${__GITDIR}")
             message(DEBUG "__GITTOP=${__GITTOP}")
