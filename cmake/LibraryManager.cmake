b38c1df7bb19d50300b402addff0fed6e7d214b3
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-11-03 09:54:45 +0000

    .

--- cmake/LibraryManager.cmake
+++ cmake/LibraryManager.cmake
@@ -570,6 +570,7 @@ macro(__LibraryManager_findBLASorLAPACK name)
                 message("testing for lapack ${name}")
                 if ("${name}" STREQUAL "LAPACK")
                     target_compile_definitions(${name}::${name} INTERFACE -DEIGEN_USE_LAPACKE)
+                    message("BLA_VENDOR_FOUND ${BLA_VENDOR_FOUND} LAPACK_LIBRARIES ${LAPACK_LIBRARIES}")
                     if ("${BLA_VENDOR_FOUND}" STREQUAL "Apple" OR "${BLA_VENDOR_FOUND}" STREQUAL "OpenBLAS" OR LAPACK_LIBRARIES MATCHES "liblapack.") # special subversion to use generic lapacke together with Accelerate or generic
                         if (APPLE AND "$ENV{HOMEBREW_PREFIX}" STREQUAL "")
                             execute_process(COMMAND brew --prefix OUTPUT_VARIABLE _homebrew_prefix)
