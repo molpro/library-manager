7f2c6b31e17a50d559fd95236f26a30da4e55d07
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-01-28 11:36:08 +0000

    more try for lapacke on macos

--- test/molpro/cblas.h
+++ test/molpro/cblas.h
@@ -9,9 +9,9 @@
 #include <mkl_cblas.h>
 #define HAVE_CBLAS
 #define HAVE_MKL
-#elif __has_include(<Accelerate/../Frameworks/vecLib.framework/Headers/cblas.h>)
-//#include "/Library/Developer/CommandLineTools/SDKs/MacOSX12.1.sdk/System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/Headers/cblas.h"
-#include <Accelerate/../Frameworks/vecLib.framework/Headers/cblas.h>
+#elif __has_include(<Accelerate/Accelerate.h>)
+#define ACCELERATE_LAPACK_ILP64
+#include <Accelerate/Accelerate.h>
 #define HAVE_CBLAS
 #define HAVE_ACCELERATE
 #elif __has_include(<cblas.h>)
